d6797fe5df3a1c7c43c07508997a726174210522
Minki-An <amk1700@gmail.com>
2023-12-27 00:44:44 +0900

    add pvc.yaml

--- nfs-server/nfs-server.sh
+++ nfs-server/nfs-server.sh
@@ -1,3 +1,17 @@
+# nfs-server
+## nfs-server install
 apt get install -y nfs-kernel-server
+## nfs 공유 디렉터리 생성
 mkdir /srv/nfs-volume
+## nfs-server 설정
 echo "/srv/nfs-volume *(rw,sync,no_subtree_check,no_root_squash)" | sudo tee /etc/exports
+
+# nfs-client
+## nfs-client install
+apt install -y nfs-common
+
+# nfs-provisioner
+## nfs-provisioner manifest file(rbac, nfs-provisioner, storageclass)
+curl https://raw.githubusercontent.com/Minki-An/YK-EFK/main/nfs-server/nfs-provision-all.yaml -o /root/nfs-provisioner.yaml
+## nfs-provisioner deploy
+kubectl apply -f /root/nfs-provisioner.yaml
